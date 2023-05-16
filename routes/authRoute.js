c0a6137c83710fdee19f55c13fbd8343b779bece
muzammildalia <muzammil.mehboob008@gmail.com>
2023-05-16 11:25:33 +0500

    first commit

---
+++ routes/authRoute.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import { forgotPasswordController, loginController, registerController, testController } from '../controllers/authControllers.js';
+import { requireSignIn, isAdmin, isUser } from '../middleware/authMiddleware.js'
+
+
+
+const router = express.Router()
+
+
+//route for REGITER in authController || POST
+router.post('/register', registerController)
+
+
+//route for login in authController || POST
+router.post('/login', loginController)
+
+//forgot password || POST
+router.post('/forgot-password', forgotPasswordController)
+
+//test route for protected
+router.get("/test", requireSignIn, isAdmin, testController);
+
+//Dashboard protected route
+router.get("/user-auth", requireSignIn, (req, res) => {
+    res.status(200).send({ ok: true });
+});
+
+export default router;
---
+++ server.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import colors from 'colors';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+import connectDb from './config/db.js';
+import authRoute from './routes/authRoute.js';
+import cors from 'cors'
+
+
+//config .env
+dotenv.config();
+
+//config database
+connectDb();
+
+//rest object
+const app = express()
+
+//middlewares
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
+
+
+//routes
+app.use("/api/v1/auth", authRoute);
+
+
+app.get('/', (req, res) => {
+    res.send({
+        message: 'Welcome to Tracking App'
+    })
+})
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+    console.log(`Server ${process.env.DEV_MODE} Running on ${PORT}`.bgCyan.white);
+})
