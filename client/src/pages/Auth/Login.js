d1c876341cee66a55a7f490ab67a4e4831d5c396
MuhammadDanishKhan16259 <muhammaddanishkhan1999@gmail.com>
2023-05-17 12:12:44 +0500

    Authentication

--- client/src/pages/Auth/Login.js
+++ client/src/pages/Auth/Login.js
@@ -1,64 +1,119 @@
-import React, { useState } from 'react'
-import Layout from '../../components/layouts/Layout'
-import { toast } from 'react-hot-toast'
-import axios from 'axios'
-import { useNavigate, useLocation, Link } from 'react-router-dom'
-import { useAuth } from '../../context/auth'
-
+import React, { useState } from "react";
+import Layout from "../../components/layouts/Layout";
+import { toast } from "react-hot-toast";
+import axios from "axios";
+import { useNavigate, useLocation, Link } from "react-router-dom";
+import { useAuth } from "../../context/auth";
+import "./style.css";
 const Login = () => {
-    const [email, setEmail] = useState("");
-    const [password, setpassword] = useState("");
-    const [auth, setAuth] = useAuth()
-    const navigate = useNavigate();
-    const location = useLocation();
+  const [email, setEmail] = useState("");
+  const [password, setpassword] = useState("");
+  const [auth, setAuth] = useAuth();
+  const navigate = useNavigate();
+  const location = useLocation();
 
+  const handlesubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post(
+        `${process.env.REACT_APP_API}/api/v1/auth/login`,
+        { email, password }
+      );
+      if (res && res.data.success) {
+        toast.success(res.data.message);
+        setAuth({
+          ...auth,
+          user: res.data.user,
+          token: res.data.token,
+        });
+        localStorage.setItem("auth", JSON.stringify(res.data));
+        navigate(location.state || "/");
+      } else {
+        toast.error(res.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went Wrong");
+    }
+  };
+  return (
+    <Layout>
+      {/* <div className="register"> */}
+      <div className="login template d-flex justify-content-center align-items-center vh-100 bg-dark bg-gradient">
+        <div className="form_container 50-w p-5 rounded bg-white">
+          {/* <h1> Login Page </h1> <br /> */}
 
-
-    const handlesubmit = async (e) => {
-        e.preventDefault();
-        try {
-            const res = await axios.post(
-                `${process.env.REACT_APP_API}/api/v1/auth/login`,
-                { email, password }
-            );
-            if (res && res.data.success) {
-                toast.success(res.data.message);
-                setAuth({
-                    ...auth,
-                    user: res.data.user,
-                    token: res.data.token
-                });
-                localStorage.setItem('auth', JSON.stringify(res.data))
-                navigate(location.state || "/");
-            }
-            else {
-                toast.error(res.data.message);
-            }
-        } catch (error) {
-            console.log(error);
-            toast.error("Something went Wrong");
-        }
-    };
-    return (
-        <Layout>
-            <div className='register'>
-                <h1> Login Page </h1> <br />
-                <form onSubmit={handlesubmit}>
-                    <div class="mb-3">
-                        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder='Email' class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required />
-                    </div>
-                    <div class="mb-1">
-                        <input type="password" value={password} onChange={(e) => setpassword(e.target.value)} placeholder='password' class="form-control" id="exampleInputPassword1" required />
-                    </div>
-                    <div class="mb-3">
-                        <button type="button" class="btn btn-primary" onClick={() => { navigate('/forgot-password'); }}>Forgot Password</button>
-                    </div>
-                    <button type="submit" class="btn btn-primary">Login</button>
-                </form>
+          <form onSubmit={handlesubmit}>
+            <h3 className="text-center"> Login Page </h3>
+            <div class="mb-2">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="Enter Email"
+                class="form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+                required
+              />
             </div>
-        </Layout>
-    )
-}
 
+            <div class="mb-2">
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setpassword(e.target.value)}
+                placeholder="Enter Password"
+                class="form-control"
+                id="exampleInputPassword1"
+                required
+              />
+            </div>
+            <div class="mb-2">
+              <input
+                type="checkbox"
+                name="custom-control custom-checkbox"
+                id="check"
+              />
+              <label htmlFor="check" className="custom-input-label ms-2">
+                Remember me
+              </label>
+            </div>
+            {/* <div class="mb-3"> */}
+            <div class="d-grid">
+              <button type="submit" className="btn btn-primary ">
+                Login
+              </button>
+              {/* <br /> */}
+              {/* <br /> */}
+              {/* 
+              <button
+                type="button"
+                class="btn btn-primary"
+                onClick={() => {
+                  navigate("/forgot-password");
+                }}
+              >
+                Forgot Password
+              </button> */}
+            </div>
+            <p className="text-end mt-2">
+              {" "}
+              Forgot
+              <Link to="/forgot-password" className="ms-2">
+                Password?
+              </Link>
+              <Link to="/Register" className="ms-2">
+                Register
+              </Link>
+            </p>
+          </form>
+        </div>
+      </div>
+    </Layout>
+  );
+};
 
-export default Login
+export default Login;
--- client/src/pages/Auth/Register.js
+++ client/src/pages/Auth/Register.js
@@ -1,74 +1,154 @@
-import React, { useState } from 'react'
-import Layout from '../../components/layouts/Layout'
-import { toast } from 'react-hot-toast'
-import axios from 'axios'
-import { useNavigate } from 'react-router-dom'
-
+import React, { useState } from "react";
+import Layout from "../../components/layouts/Layout";
+import { toast } from "react-hot-toast";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+import "./style.css";
 const Register = () => {
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setpassword] = useState("");
-    const [phone, setPhone] = useState("");
-    const [address, setAddress] = useState("");
-    const [answer, setAnswer] = useState("");
-    const navigate = useNavigate();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setpassword] = useState("");
+  const [phone, setPhone] = useState("");
+  const [address, setAddress] = useState("");
+  const [answer, setAnswer] = useState("");
+  const navigate = useNavigate();
 
-    const handlesubmit = async (e) => {
-        e.preventDefault();
-        try {
-            const res = await axios.post(
-                `${process.env.REACT_APP_API}/api/v1/auth/register`,
-                { name, email, password, phone, address, answer }
-            );
-            if (res && res.data.success) {
-
-                navigate("/login");
-                toast.success(res.data.message);
-            }
-            else {
-                toast.error(res.data.message);
-            }
-        } catch (error) {
-            console.log(error);
-            toast.error("Something went Wrong");
-        }
-    };
-    return (
-        <Layout>
-            <div className='register'>
-                <h1> Register Page </h1>
-                <form onSubmit={handlesubmit}>
-                    <div class="mb-3">
-                        <input type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder='Name' class="form-control" id="exampleInputPassword1" required />
-                    </div>
-                    <div class="mb-3">
-                        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder='Email' class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required />
-                    </div>
-                    <div class="mb-3">
-                        <input type="password" value={password} onChange={(e) => setpassword(e.target.value)} placeholder='password' class="form-control" id="exampleInputPassword1" required />
-                    </div>
-                    <div class="mb-3">
-                        <input type="text" value={phone} onChange={(e) => setPhone(e.target.value)} placeholder='phone No.' class="form-control" id="exampleInputPassword1" required />
-                    </div>
-                    <div class="mb-3">
-                        <input type="text" value={address} onChange={(e) => setAddress(e.target.value)} placeholder='Address' class="form-control" id="exampleInputPassword1" required />
-                    </div>
-                    <div class="mb-3">
-                        <div class="form-floating">
-                            <select class="form-select" id="floatingSelectGrid" aria-label="Floating label select example" required >
-                                <option selected></option>
-                                <option value="1">Your Favourite Teacher Name</option>
-                                <option value="2">Your Favourite Sports</option>
-                            </select>
-                            <label for="floatingSelectGrid">Questions For Verification</label>
-                        </div>
-                        <input type="text" value={answer} onChange={(e) => setAnswer(e.target.value)} placeholder='Answer' class="form-control" id="exampleInputPassword1" required />
-                    </div>
-                    <button type="submit" class="btn btn-primary">Register</button>
-                </form>
+  const handlesubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post(
+        `${process.env.REACT_APP_API}/api/v1/auth/register`,
+        { name, email, password, phone, address, answer }
+      );
+      if (res && res.data.success) {
+        navigate("/login");
+        toast.success(res.data.message);
+      } else {
+        toast.error(res.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went Wrong");
+    }
+  };
+  return (
+    <Layout>
+      {/* <div className='register'> */}
+      <div className="signup template d-flex justify-content-center align-items-center vh-70 bg-primary">
+        <div className="form_container 50-w p-5 rounded bg-white">
+          {/* <h1> Register Page </h1> */}
+          <form onSubmit={handlesubmit}>
+            <h3 className="text-center"> Register Page </h3>
+            <label htmlFor="name">Name</label>
+            <div class="mb-2">
+              <input
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                placeholder="Enter Name"
+                class="form-control"
+                id="exampleInputPassword1"
+                required
+              />
             </div>
-        </Layout>
-    )
-}
+            <div class="mb-2">
+              <label htmlFor="email">Email</label>
 
-export default Register
+              <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="Email"
+                class="form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+                required
+              />
+            </div>
+            <div class="mb-2">
+              <label htmlFor="password">Password</label>
+
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setpassword(e.target.value)}
+                placeholder="password"
+                class="form-control"
+                id="exampleInputPassword1"
+                required
+              />
+            </div>
+            <div class="mb-2">
+              <label htmlFor="phone">Phone</label>
+
+              <input
+                type="text"
+                value={phone}
+                onChange={(e) => setPhone(e.target.value)}
+                placeholder="phone No."
+                class="form-control"
+                id="exampleInputPassword1"
+                required
+              />
+            </div>
+            <div class="mb-2">
+              <label htmlFor="address">Address</label>
+              <input
+                type="text"
+                value={address}
+                onChange={(e) => setAddress(e.target.value)}
+                placeholder="Address"
+                class="form-control"
+                id="exampleInputPassword1"
+                required
+              />
+            </div>
+            <div class="mb-2">
+              <label htmlFor="option">Select any Option Given Below</label>
+
+              <div class="form-floating">
+                <select
+                  class="form-select"
+                  id="floatingSelectGrid"
+                  aria-label="Floating label select example"
+                  required
+                >
+                  <option selected></option>
+                  <option value="1">Your Favourite Teacher Name</option>
+                  <option value="2">Your Favourite Sports</option>
+                </select>
+                <label for="floatingSelectGrid">
+                  Questions For Verification
+                </label>
+              </div>
+              <label htmlFor="answer">Type Your seleted Option</label>
+
+              <input
+                type="text"
+                value={answer}
+                onChange={(e) => setAnswer(e.target.value)}
+                placeholder="Answer"
+                class="form-control"
+                id="exampleInputPassword1"
+                required
+              />
+            </div>
+            <div class="d-grid  mt-2">
+              <button type="submit" class="btn btn-primary">
+                Register
+              </button>
+            </div>
+            <p className="text-end mt-2">
+              Already Registered
+              <Link to="/login" className="ms-2">
+                Login
+              </Link>
+            </p>
+          </form>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default Register;
