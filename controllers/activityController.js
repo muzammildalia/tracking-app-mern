36a4140b46c014aa863f4b0e08dfe8e30c3ad55c
Muzammil Dalia <dailamuz@corp.contoursoftware.net>
2023-05-18 18:16:46 +0500

    hello

--- controllers/activityController.js
+++ controllers/activityController.js
@@ -45,7 +45,7 @@ export const createActivityController = async (req, res) => {
         console.log(error);
         res.status(500).send({
             success: false,
-            message: "Error in creating activity",
+            message: "Error in creatings activity",
             error,
         })
     }
